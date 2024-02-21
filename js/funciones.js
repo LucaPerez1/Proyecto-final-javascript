920fef949a178ce65ed6633778524c57ec5e3ab5
Luca <lucaperez2012@gmail.com>
2024-02-21 16:19:50 -0300

    reder productos + prodcutos

--- js/funciones.js
+++ js/funciones.js
@@ -7,6 +7,10 @@ const productos = [
     { id: 6, nombre: "Cerveza Brahama Chopp lata 473ml x6 unidades", precio: "$10.500,00", imagen: "../img/Brahma.jpg" },
     { id: 7, nombre: "Fernet branca 750ml + 2 cocas 2.25ml", precio: "$11.000,00", imagen: "../img/Fernet con coca.jpg" },
     { id: 8, nombre: "Vodka Skyy durazno 750ml", precio: "$6.000,00", imagen: "../img/Vodka-Skyy.jpg" },
+    { id: 9, nombre: "Cerveza Heineken Rubia lata 473ml x6 unidades",precio: "$8.550,00", imagen: "../img/heineken.png"},
+    { id: 10, nombre: "Whisky Jhonny Walker Blue Label 750ml",precio: "$250.000,00", imagen: "../img/blue-label.png"},
+    { id: 11, nombre: "Gin spirito blue 700ml",precio: "$8.800,00", imagen: "../img/gin-spirito-blue.png"},
+    { id: 12, nombre: "Cerveza Corona American Adjunct Lager 330mL x6 unidades",precio: "$7.300,00", imagen: "../img/coronax6.jpg"},
 ]
 
 //Funciones local storage productos
